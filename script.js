52d03cc207613b8dacf9880e97122f4a6dbd83ea
phideptrai123 <trantenphi.1@gmail.com>
2025-08-07 17:03:47 +0700

    script.js

--- script.js
+++ script.js
@@ -49,16 +49,4 @@ function showMessage(){
 }
 
 
-import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyCq7zMGmgl2M0Ueyon3zBCU1WwzxXYtKLg");
-
-async function main() {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  const result = await model.generateContent("Explain how AI works in a few words");
-  const response = await result.response;
-  console.log(response.text());
-}
-
-main();
